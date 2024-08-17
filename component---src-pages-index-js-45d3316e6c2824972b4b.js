262e285ef974b45b6eb38c5241ae6f57f19b462e
Gabriel Alvarado <gabriel.alvmart@gmail.com>
2024-08-16 22:56:43 -0600

    Updates

---
+++ component---src-pages-index-js-45d3316e6c2824972b4b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[293],{9639:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(6540),a=n(7502);function i(){return r.createElement(r.Fragment,null,r.createElement(a.G$,{title:"Dantepon"}),r.createElement(a.YW,{useSplashScreenAnimation:!0},r.createElement(a.KE,{sectionId:"hero"}),r.createElement(a.nr,{sectionId:"about",heading:"About me"})))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-45d3316e6c2824972b4b.js.map
